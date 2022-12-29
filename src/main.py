5b7827b567786c4e59c22a166037097f6aa1e432
Afonso Corte-Real <afonsocortereal15@gmail.com>
2022-12-29 11:39:58 +0000

    Change the arrangement of the numbers on the grid

--- src/main.py
+++ src/main.py
@@ -101,7 +101,6 @@ expression_field.grid(columnspan=4, ipadx=154)
 Grid.rowconfigure(gui,(0, 1, 2, 3, 4, 5),weight=1)
 Grid.columnconfigure(gui,(0, 1, 2, 3),weight=1)
 
-
 # clear button
 button_clear = Button(gui, text='Clear', fg='white', bg='#454545', font='Arial 20', relief=RIDGE,
                 command=clear, height=2, width=14)
@@ -117,83 +116,78 @@ button_divide = Button(gui, text='÷', fg='white', bg='#454545', font='Arial 20'
                 command=lambda: press("/"), height=2, width=14)
 button_divide.grid(row=1, column=3, sticky='NSEW')
 
-# 1 button
-button1 = Button(gui, text=' 1 ', fg='white', bg='#454545', font='Arial 20', relief=RIDGE,
-                    command=lambda: press(1), height=2, width=14)
-button1.grid(row=2, column=0, sticky='NSEW')
+# button 7
+button7 = Button(gui, text='7', fg='white', bg='#454545', font='Arial 20', relief=RIDGE,
+                command=lambda: press(7), height=2, width=14)
+button7.grid(row=2, column=0, sticky='NSEW')
 
-# 2 button
-button2 = Button(gui, text=' 2 ', fg='white', bg='#454545', font='Arial 20', relief=RIDGE,
-                    command=lambda: press(2), height=2, width=14)
-button2.grid(row=2, column=1, sticky='NSEW')
+# button 8
+button8 = Button(gui, text='8', fg='white', bg='#454545', font='Arial 20', relief=RIDGE,
+                command=lambda: press(8), height=2, width=14)
+button8.grid(row=2, column=1, sticky='NSEW')
 
-# 3 button
-button3 = Button(gui, text=' 3 ', fg='white', bg='#454545', font='Arial 20', relief=RIDGE,
-                    command=lambda: press(3), height=2, width=14)
-button3.grid(row=2, column=2, sticky='NSEW')
+# button 9
+button9 = Button(gui, text='9', fg='white', bg='#454545', font='Arial 20', relief=RIDGE,
+                command=lambda: press(9), height=2, width=14)
+button9.grid(row=2, column=2, sticky='NSEW')
 
 # multiply button
-button_multiply = Button(gui, text=' x ', fg='white', bg='#454545', font='Arial 20', relief=RIDGE,
-                    command=lambda: press("*"), height=2, width=14)
+button_multiply = Button(gui, text='x', fg='white', bg='#454545', font='Arial 20', relief=RIDGE,
+                command=lambda: press('*'), height=2, width=14)
 button_multiply.grid(row=2, column=3, sticky='NSEW')
 
-# 4 button
-button4 = Button(gui, text=' 4 ', fg='white', bg='#454545', font='Arial 20', relief=RIDGE,
-                    command=lambda: press(4), height=2, width=14)
+# button 4
+button4 = Button(gui, text='4', fg='white', bg='#454545', font='Arial 20', relief=RIDGE,
+                command=lambda: press(4), height=2, width=14)
 button4.grid(row=3, column=0, sticky='NSEW')
 
-# 5 button
-button5 = Button(gui, text=' 5 ', fg='white', bg='#454545', font='Arial 20', relief=RIDGE,
-                    command=lambda: press(5), height=2, width=14)
+# button 5
+button5 = Button(gui, text='5', fg='white', bg='#454545', font='Arial 20', relief=RIDGE,
+                command=lambda: press(5), height=2, width=14)
 button5.grid(row=3, column=1, sticky='NSEW')
 
-# 6 button
-button6 = Button(gui, text=' 6 ', fg='white', bg='#454545', font='Arial 20', relief=RIDGE,
-                    command=lambda: press(6), height=2, width=14)
+# button 6
+button6 = Button(gui, text='6', fg='white', bg='#454545', font='Arial 20', relief=RIDGE,
+                command=lambda: press(6), height=2, width=14)
 button6.grid(row=3, column=2, sticky='NSEW')
 
-# minus button
-button_minus = Button(gui, text=' - ', fg='white', bg='#454545', font='Arial 20', relief=RIDGE,
-                command=lambda: press("-"), height=2, width=14)
-button_minus.grid(row=3, column=3, sticky='NSEW')
+# subtract button
+button_subtract = Button(gui, text='-', fg='white', bg='#454545', font='Arial 20', relief=RIDGE,
+                command=lambda: press('-'), height=2, width=14)
+button_subtract.grid(row=3, column=3, sticky='NSEW')
 
-# 7 button
-button7 = Button(gui, text=' 7 ', fg='white', bg='#454545', font='Arial 20', relief=RIDGE,
-                    command=lambda: press(7), height=2, width=14)
-button7.grid(row=4, column=0, sticky='NSEW')
+# button 1
+button1 = Button(gui, text='1', fg='white', bg='#454545', font='Arial 20', relief=RIDGE,
+                command=lambda: press(1), height=2, width=14)
+button1.grid(row=4, column=0, sticky='NSEW')
 
-# 8 button
-button8 = Button(gui, text=' 8 ', fg='white', bg='#454545', font='Arial 20', relief=RIDGE,
-                    command=lambda: press(8), height=2, width=14)
-button8.grid(row=4, column=1, sticky='NSEW')
+# button 2
+button2 = Button(gui, text='2', fg='white', bg='#454545', font='Arial 20', relief=RIDGE,
+                command=lambda: press(2), height=2, width=14)
+button2.grid(row=4, column=1, sticky='NSEW')
 
-# 9 button
-button9 = Button(gui, text=' 9 ', fg='white', bg='#454545', font='Arial 20', relief=RIDGE,
-                    command=lambda: press(9), height=2, width=14)
-button9.grid(row=4, column=2, sticky='NSEW')
+# button 3
+button3 = Button(gui, text='3', fg='white', bg='#454545', font='Arial 20', relief=RIDGE,
+                command=lambda: press(3), height=2, width=14)
+button3.grid(row=4, column=2, sticky='NSEW')
 
-# plus button
-button_plus = Button(gui, text=' + ', fg='white', bg='#454545', font='Arial 20', relief=RIDGE,
-                command=lambda: press("+"), height=2, width=14)
-button_plus.grid(row=4, column=3, sticky='NSEW')
-
-# squared button
-button_squared = Button(gui, text=' x^y ', fg='white', bg='#454545', font='Arial 20', relief=RIDGE,
-                    command=lambda: press('**'), height=2, width=14)
-button_squared.grid(row=5, column=0, sticky='NSEW')
+# sum button
+button_sum = Button(gui, text='+', fg='white', bg='#454545', font='Arial 20', relief=RIDGE,
+                command=lambda: press('+'), height=2, width=14)
+button_sum.grid(row=4, column=3, sticky='NSEW')
 
 # button 0
-button0 = Button(gui, text=' 0 ', fg='white', bg='#454545', font='Arial 20', relief=RIDGE,
-                    command=lambda: press(0), height=2, width=14)
+button0 = Button(gui, text='0', fg='white', bg='#454545', font='Arial 20', relief=RIDGE,
+                command=lambda: press(0), height=2, width=14)
 button0.grid(row=5, column=1, sticky='NSEW')
 
-# , button
-button_comma = Button(gui, text=' , ', fg='white', bg='#454545', font='Arial 20', relief=RIDGE,
-                    command=lambda: press('.'), height=2, width=14)
+# comma button
+button_comma = Button(gui, text=',', fg='white', bg='#454545', font='Arial 20', relief=RIDGE,
+                command=lambda: press('.'), height=2, width=14)
 button_comma.grid(row=5, column=2, sticky='NSEW')
 
 # equal button
-button_equal = Button(gui, text=' = ', fg='white', bg='#454545', font='Arial 20', relief=RIDGE,
+button_equal = Button(gui, text='=', fg='white', bg='#454545', font='Arial 20', relief=RIDGE,
                 command=equalpress, height=2, width=14)
 button_equal.grid(row=5, column=3, sticky='NSEW')
 #endregion
@@ -251,7 +245,6 @@ def pressed_key(key):
 
     else:
         ''
-    
 
         
 keyboard.on_press(pressed_key)
